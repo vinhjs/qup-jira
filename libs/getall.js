3b11a1bb6be687403d782a808a5cf251c8071063
Vinh Tran <vinhjs@gmail.com>
2017-11-07 23:35:10 +0700

    full

--- libs/getall.js
+++ libs/getall.js
@@ -1,65 +1,53 @@
-module.exports = function(params){
-    var request = require('request');
-    var prompt = require('prompt');
-    var async = require('async');
-    var Excel = require('exceljs');
-    var schema = {
-        properties: {
-            status: {
-                default: 'all'
-            },
-            excel: {
-                description: 'input csv name',
-                default: 'full.xlsx'
-            }
+var request = require('request');
+var prompt = require('prompt');
+var async = require('async');
+var Excel = require('exceljs');
+var schema = {
+    properties: {
+        status: {
+            default: 'all'
+        },
+        excel: {
+            description: 'input csv name',
+            default: 'full.xlsx'
         }
-    };
+    }
+};
+var options = {
+    filename:  './full.xlsx',
+    useStyles: true,
+    useSharedStrings: true
+};
+var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
+workbook.creator = 'Me';
+workbook.lastModifiedBy = 'Me';
+workbook.created = new Date();
+workbook.modified = new Date();
+workbook.views = [
+    {
+    x: 0, y: 0, width: 10000, height: 20000,
+    firstSheet: 0, activeTab: 0, visibility: 'visible'
+    }
+];
+var array_sheet = ['FULL', 'vinh.tran', 'thuan.ho', 'trinh.nguyent', 'song.truong', 'quynh.hoang', 'phuong.tran', 'nguyen.tran', 'minh.nguyen', 'linh.tranh', 'hoang.nguyen', 'hao.le', 'duy.phan', 'dat.pham', 'dat.huynh', 'chuong.vo', 'chuong.nguyen'];
+var worksheets = {};
+var row_count = {};
+array_sheet.forEach(function(sheetName){
+    addWorkSheet(workbook, sheetName);
+    worksheets[sheetName] = workbook.getWorksheet(sheetName);
+    row_count[sheetName] = 1;
+})
+module.exports = function(params){
+    
     prompt.start();
     prompt.get(schema, function (err, result) {
         var startAt = 0;
-        var total = 50
-        var count = 1;
-        var options = {
-            filename:  './'+result.excel,
-            useStyles: true,
-            useSharedStrings: true
-        };
-        var workbook = new Excel.stream.xlsx.WorkbookWriter(options);
-        workbook.creator = 'Me';
-        workbook.lastModifiedBy = 'Me';
-        workbook.created = new Date();
-        workbook.modified = new Date();
-        workbook.views = [
-            {
-            x: 0, y: 0, width: 10000, height: 20000,
-            firstSheet: 0, activeTab: 1, visibility: 'visible'
-            }
-        ];
-        workbook.addWorksheet('FULL', {properties: {tabColor: {argb: 'FFC0000'}}});
-        var worksheet = workbook.getWorksheet('FULL');
-        var columns = [
-            { header: 'Key', width: 30 },
-            { header: 'Summary', width: 50 },
-            { header: 'Type', width: 17 },
-            { header: 'ETA-Duedate', width: 17 },
-            { header: 'Assignee', width: 17 },
-            { header: 'Status', width: 10 },
-        ]
-        worksheet.columns = columns;
-        var headerRow = worksheet.getRow(1)
-        headerRow.fill = {
-            type: 'pattern',
-            pattern:'solid',
-            fgColor:{argb:'a7a4a3'}
-        };
-        headerRow.height = 30;
-        headerRow.alignment = { vertical: 'middle', horizontal: 'center' };
-        headerRow.font = {bold: true, size: 12, color: {argb: 'ffffff'}};
+        var total = 50  
         async.whilst(
             function () { return startAt < total; },
             function (callback) {
                request({
-                    url: 'https://issues.qup.vn/rest/api/2/search?startAt='+startAt+'&jql=key in (QTX-19439, QTX-19865, QTX-19511, QTX-19512, QTX-19513, QTX-19509, QTX-19561, QTX-19759, QTX-19758, QTX-17652, QTX-19154, QTX-19183, QTX-19754, QTX-19753, QTX-19562, QTX-19563, QTX-19614, QTX-19615, QTX-19871, QTX-19916, QTX-19370, QTX-19609, QTX-19610, QTX-19704, QTX-19705, QTX-19855, QTX-19663, QTX-19917, QTX-19574, QTX-19918, QTX-18890, QTX-19846, QTX-19774, QTX-17782, QTX-17826, QTX-16252, QTX-16385, QTX-19868, QTX-18544, QTX-17232, QTX-17338, QTX-19749, QTX-19731, QTX-18876, QTX-18914, QTX-18938, QTX-19864, QTX-19838, QTX-19850, QTX-19942, QTX-19943)',
+                    url: 'https://issues.qup.vn/rest/api/2/search?startAt='+startAt+'&jql=key in (QTX-19439, QTX-19865, QTX-19511, QTX-19512, QTX-19513, QTX-19509, QTX-19561, QTX-19759, QTX-19758, QTX-17652, QTX-19154, QTX-19183, QTX-19754, QTX-19753, QTX-19562, QTX-19563, QTX-19614, QTX-19615, QTX-19871, QTX-19916, QTX-19370, QTX-19609, QTX-19610, QTX-19704, QTX-19705, QTX-19855, QTX-19663, QTX-19917, QTX-19574, QTX-19918, QTX-18890, QTX-19846, QTX-19774, QTX-17782, QTX-17826, QTX-16252, QTX-16385, QTX-19868, QTX-18544, QTX-17338, QTX-19749, QTX-19731, QTX-18914, QTX-18938, QTX-19864, QTX-19838, QTX-19850, QTX-19942, QTX-19943)',
                     timeout: 10000,
                     json: true,
                     'auth': {
@@ -72,13 +60,16 @@ module.exports = function(params){
                         total = result.total;
                         startAt += 50;
                         async.forEachLimit(result.issues, 1, function(issue, cback){  
-                            var row = worksheet.getRow(++count);
-                            row.getCell(1).value = { text: issue.key, hyperlink: 'https://issues.qup.vn/browse/' + issue.key };
-                            row.getCell(2).value =issue.fields.summary;
-                            row.getCell(3).value =issue.fields.issuetype.name;
-                            row.getCell(4).value =issue.fields.timeestimate + '-' + issue.fields.duedate;                            
-                            row.getCell(5).value = '';                                                                                
-                            row.getCell(6).value = '';                                                                                
+                            var rowData = {
+                                key: { text: issue.key, hyperlink: 'https://issues.qup.vn/browse/' + issue.key },
+                                summary: issue.fields.summary,
+                                issuetype: issue.fields.issuetype.name,
+                                eta: issue.fields.timeestimate + '-' + issue.fields.duedate,
+                                assignee: issue.fields.assignee.name,
+                                status: issue.fields.status.name,
+                            }
+                            setRow('FULL', rowData);  
+                            setRow(rowData.assignee, rowData);                                                                                                          
                             if (issue.fields.issuetype.name == 'Task' && issue.fields.issuelinks.length) { 
                                 var keys = '';                              
                                 async.forEach(issue.fields.issuelinks, function(linkIssue, cback){
@@ -105,14 +96,16 @@ module.exports = function(params){
                                             if (result && result.total) {
                                                 async.forEachLimit(result.issues, 1, function(issueDev, cback){  
                                                     if (issueDev.fields.subtasks.length) {
-                                                        var row = worksheet.getRow(++count);                                                        
-                                                        row.getCell(1).value = { text: issue.key + ' => ' + issueDev.key, hyperlink: 'https://issues.qup.vn/browse/' + issueDev.key };
-                                                        row.getCell(2).value =issueDev.fields.summary;
-                                                        row.getCell(3).value =issueDev.fields.issuetype.name;
-                                                        row.getCell(4).value =issueDev.fields.timeestimate + '-' + issueDev.fields.duedate;                            
-                                                        row.getCell(5).value = issueDev.fields.assignee.name;
-                                                        row.getCell(6).value = issueDev.fields.status.name;
-                                                        row.commit();
+                                                        var rowData = {
+                                                            key: { text: issue.key + ' => ' + issueDev.key, hyperlink: 'https://issues.qup.vn/browse/' + issueDev.key },
+                                                            summary: issueDev.fields.summary,
+                                                            issuetype: issueDev.fields.issuetype.name,
+                                                            eta: issueDev.fields.timeestimate + '-' + issueDev.fields.duedate,
+                                                            assignee: issueDev.fields.assignee.name,
+                                                            status: issueDev.fields.status.name,
+                                                        }
+                                                        setRow('FULL', rowData); 
+                                                        setRow(rowData.assignee, rowData); 
                                                         var subTasksKeys = '';
                                                         async.forEach(issueDev.fields.subtasks, function(subTask, cback){
                                                             subTasksKeys += ',' + subTask.key;
@@ -132,14 +125,16 @@ module.exports = function(params){
                                                                     console.log('request dev-subtasks done', subTasksKeys);
                                                                     if (result && result.total) {
                                                                         async.forEach(result.issues, function(subTaskDev, cback){  
-                                                                            var row = worksheet.getRow(++count);
-                                                                            row.getCell(1).value = { text: issue.key + ' => ' + issueDev.key + ' => ' + subTaskDev.key, hyperlink: 'https://issues.qup.vn/browse/' + subTaskDev.key };
-                                                                            row.getCell(2).value =subTaskDev.fields.summary;
-                                                                            row.getCell(3).value =subTaskDev.fields.issuetype.name;
-                                                                            row.getCell(4).value =subTaskDev.fields.timeestimate + '-' + subTaskDev.fields.duedate;                            
-                                                                            row.getCell(5).value = subTaskDev.fields.assignee.name;
-                                                                            row.getCell(6).value = subTaskDev.fields.status.name;
-                                                                            row.commit();
+                                                                            var rowData = {
+                                                                                key: { text: issue.key + ' => ' + issueDev.key + ' => ' + subTaskDev.key, hyperlink: 'https://issues.qup.vn/browse/' + subTaskDev.key },
+                                                                                summary: subTaskDev.fields.summary,
+                                                                                issuetype: subTaskDev.fields.issuetype.name,
+                                                                                eta: subTaskDev.fields.timeestimate + '-' + subTaskDev.fields.duedate,
+                                                                                assignee: subTaskDev.fields.assignee.name,
+                                                                                status: subTaskDev.fields.status.name,
+                                                                            }
+                                                                            setRow('FULL', rowData);
+                                                                            setRow(rowData.assignee, rowData); 
                                                                             cback();
                                                                         }, function(){
                                                                             cback();
@@ -153,14 +148,16 @@ module.exports = function(params){
                                                             }
                                                         })
                                                     } else {
-                                                        var row = worksheet.getRow(++count);
-                                                        row.getCell(1).value = { text: issue.key + ' => ' + issueDev.key, hyperlink: 'https://issues.qup.vn/browse/' + issueDev.key };
-                                                        row.getCell(2).value =issueDev.fields.summary;
-                                                        row.getCell(3).value =issueDev.fields.issuetype.name;
-                                                        row.getCell(4).value =issueDev.fields.timeestimate + '-' + issueDev.fields.duedate;                            
-                                                        row.getCell(5).value = issueDev.fields.assignee.name;
-                                                        row.getCell(6).value = issueDev.fields.status.name;
-                                                        row.commit();
+                                                        var rowData = {
+                                                            key: { text: issue.key + ' => ' + issueDev.key, hyperlink: 'https://issues.qup.vn/browse/' + issueDev.key },
+                                                            summary: issueDev.fields.summary,
+                                                            issuetype: issueDev.fields.issuetype.name,
+                                                            eta: issueDev.fields.timeestimate + '-' + issueDev.fields.duedate,
+                                                            assignee: issueDev.fields.assignee.name,
+                                                            status: issueDev.fields.status.name,
+                                                        }
+                                                        setRow('FULL', rowData);
+                                                        setRow(rowData.assignee, rowData); 
                                                         cback();
                                                     }                                                    
                                                 }, function(){
@@ -176,9 +173,6 @@ module.exports = function(params){
                                     }
                                 }) 
                             } else {
-                                row.getCell(5).value =issue.fields.assignee.name;
-                                row.getCell(6).value =issue.fields.status.name;
-                                row.commit(); 
                                 cback(); 
                             }                           
                         }, function(){
@@ -204,4 +198,43 @@ module.exports = function(params){
         );
         
     });
+}
+function addWorkSheet(workbook, sheetName){
+    workbook.addWorksheet(sheetName, {properties: {tabColor: {argb: 'FFC0000'}}});
+    var worksheet = workbook.getWorksheet(sheetName);
+    var columns = [
+        { header: 'Key', width: 30 },
+        { header: 'Summary', width: 50 },
+        { header: 'Type', width: 17 },
+        { header: 'ETA-Duedate', width: 17 },
+        { header: 'Assignee', width: 17 },
+        { header: 'Status', width: 10 },
+    ]
+    worksheet.columns = columns;
+    var headerRow = worksheet.getRow(1)
+    headerRow.fill = {
+        type: 'pattern',
+        pattern:'solid',
+        fgColor:{argb:'a7a4a3'}
+    };
+    headerRow.height = 30;
+    headerRow.alignment = { vertical: 'middle', horizontal: 'center' };
+    headerRow.font = {bold: true, size: 12, color: {argb: 'ffffff'}};
+}
+function setRow(sheetName, data){
+    var row = worksheets[sheetName].getRow(++row_count[sheetName]);
+    row.getCell(1).value = data.key;
+    row.getCell(2).value = data.summary;
+    row.getCell(3).value = data.issuetype;
+    row.getCell(4).value = data.eta;                            
+    row.getCell(5).value = data.assignee;                                                                                
+    row.getCell(6).value = data.status;
+    if (data.status == 'Resolved') {
+        row.fill = {
+        type: 'pattern',
+        pattern:'solid',
+        fgColor:{argb:'00FFFF'}
+    };
+    }
+    row.commit(); 
 }
